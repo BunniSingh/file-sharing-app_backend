cad49fdf4ddc9b7d8ff9b5b8e8b910efb262419b
Banti Kumar Singh <bantikumarsingh91@gmail.com>
2025-03-10 23:18:30 +0530

    .env file added

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require("cors");
+require('dotenv').config();
 
 const fileRoute = require('./route/file.route');
 
 const app = express();
-const port = 5500;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/v1/file", fileRoute)
 mongoose
-    .connect('mongodb+srv://bantikumarsingh97:lJ2dl5u8N6gVw1hg@cluster0.t9ati.mongodb.net/file_sharing_app')
+    .connect(`${process.env.DB_CONNECTION_STRING}/file_sharing_app`)
     .then(() => console.log("DB Connected Successfully"))
     .catch(err => console.log("Error while connection DB", err));
 
